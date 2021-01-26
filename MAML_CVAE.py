6836eb64a1b988f3d6f854aad21632f066b34ead
xuchen <chen.xu@geelink.cn>
2021-01-26 17:01:23 +0800

    print the whole loss

--- MAML_CVAE.py
+++ MAML_CVAE.py
@@ -2,22 +2,14 @@ import matplotlib
 
 matplotlib.use('Agg')
 from utils.data_reader import Personas_CVAE
-from model.transformer import Transformer
 from model.common_layer import NoamOpt, evaluate
 from utils import config
 from model.CVAE.util.config import Model_Config
 import torch
 import torch.nn as nn
-from model.CVAE.Optim import Optim
-import torch.nn.functional as F
-from tqdm import tqdm
-import os
-import time
 import numpy as np
 from random import shuffle
 from copy import deepcopy
-import matplotlib.pyplot as plt
-import seaborn as sns
 import math
 from tensorboardX import SummaryWriter
 import os
@@ -82,35 +74,39 @@ def do_learning_early_stop(model, train_iter, val_iter, iterations, strict=1):
 
 
 def do_learning_fix_step(model, train_iter, val_iter, iterations, test=False):
-    val_p = []
-    val_p_list = []
-    val_loss = 0
-    for _, _ in enumerate(range(iterations)):
+    # fine-tuning and calculate loss
+    ppl_list = []
+    nll_list = []
+    kld_list = []
+    weight_list = []
+    loss_sum = 0
 
-        for d in train_iter:
-            t_loss, t_ppl, _ = model.train_one_batch(d)
-        if test:
-            _, test_ppl = do_evaluation(model, val_iter)
-            val_p_list.append(test_ppl)
-    # weight = deepcopy(model.state.dict())
+    for _ in range(iterations):
+        for batch in train_iter:
+            model.train_one_batch(batch)
 
-    if test:
-        return val_p_list
-    else:
-        for d in val_iter:
-            _, t_ppl, t_loss = model.train_one_batch(d, train=False)
-            val_loss += t_loss
-            val_p.append(t_ppl)
-        return val_loss, np.mean(val_p)
+    i = 0
+    for batch in val_iter:
+        _, ppl, loss, nll_loss, kld_loss, kld_weight = model.train_one_batch(batch, train=False)
+        loss_sum += loss
+        ppl_list.append(ppl)
+        nll_list.append(nll_loss)
+        kld_list.append(kld_loss)
+        weight_list.append(kld_weight)
+        i = i + 1
+    return loss_sum / i, np.mean(ppl_list), np.mean(nll_list), np.mean(kld_list), np.mean(weight_list)
 
 
 def do_evaluation(model, test_iter):
-    p, l = [], []
+    p, l, nll, kl, w = [], [], [], [], []
     for batch in test_iter:
-        loss, ppl, _ = model.train_one_batch(batch, train=False)
+        loss, ppl, _, nll_loss, kl_loss, weight = model.train_one_batch(batch, train=False)
         l.append(loss)
         p.append(ppl)
-    return np.mean(l), np.mean(p)
+        nll.append(nll_loss)
+        kl.append(kl_loss)
+        w.append(weight)
+    return np.mean(l), np.mean(p), np.mean(nll), np.mean(kl), np.mean(w)
 
 
 # =================================main=================================
@@ -141,16 +137,16 @@ elif config.meta_optimizer == 'noam':
 else:
     raise ValueError
 
-
+global_step = 0
 meta_batch_size = config.meta_batch_size
 tasks = p.get_personas('train')
 # tasks_loader = {t: p.get_data_loader(persona=t,batch_size=config.batch_size, split='train') for t in tasks}
 tasks_iter = make_infinite_list(tasks)
 
 # meta early stop
-patience = 50
+patience = 150
 if config.fix_dialnum_train:
-    patience = 100
+    patience = 300
 # best_loss = 10000000
 best_loss = 100.0
 stop_count = 0
@@ -173,12 +169,13 @@ for meta_iteration in range(config.epochs):
             train_iter, val_iter = p.get_data_loader(persona=tasks_iter.__next__(), batch_size=config.batch_size,
                                                      split='train')
         # before first update
-        v_loss, v_ppl = do_evaluation(meta_net, val_iter)
-        train_loss_before.append(math.exp(v_loss))
+        v_loss, v_ppl, v_nll, v_kl, v_w = do_evaluation(meta_net, val_iter)
+        train_loss_before.append(v_ppl)
         batch_loss_before += v_loss
-        # Update fast nets   
-        val_loss, v_ppl = do_learning_fix_step(meta_net, train_iter, val_iter, iterations=config.meta_iteration)
-        train_loss_meta.append(math.exp(val_loss.item()))
+        # Update fast nets
+        val_loss, val_ppl, _, _, _ = do_learning_fix_step(meta_net, train_iter, val_iter,
+                                                          iterations=config.meta_iteration)
+        train_loss_meta.append(val_ppl)
         batch_loss += val_loss
         # log
 
@@ -190,7 +187,7 @@ for meta_iteration in range(config.epochs):
     # print(' loss_before: {}'.format(np.mean(train_loss_before)))
     # print(' loss_meta: {}'.format(np.mean(np.mean(train_loss_meta))))
 
-    meta_net.optim.update_lr(meta_iteration)  # 每个epoch更新学习率
+    # meta_net.optim.update_lr(global_step)  # 每个epoch更新学习率
 
     # meta Update
     if (config.meta_optimizer == 'noam'):
@@ -201,6 +198,7 @@ for meta_iteration in range(config.epochs):
     batch_loss_before /= meta_batch_size
     batch_loss /= meta_batch_size
     batch_loss.backward()
+    meta_net.global_step += 1
     print('train_loss: {:.3f} ===> {:.3f}'.format(batch_loss_before, batch_loss))
 
     # clip gradient
@@ -215,6 +213,12 @@ for meta_iteration in range(config.epochs):
         val_loss_meta = []
         val_batch_loss_before = 0
         val_batch_loss_after = 0
+        val_nll_loss_before = 0
+        val_nll_loss_after = 0
+        val_kld_loss_before = 0
+        val_kld_loss_after = 0
+        val_kld_weight_before = 0
+        val_kld_weight_after = 0
         weights_original = deepcopy(meta_net.state_dict())
         for idx, per in enumerate(p.get_personas('valid')):
             # num_of_dialog = p.get_num_of_dialog(persona=per, split='valid')
@@ -227,13 +231,21 @@ for meta_iteration in range(config.epochs):
                 train_iter, val_iter = p.get_data_loader(persona=per, batch_size=config.batch_size, split='valid',
                                                          fold=0)
             # zero shot result
-            loss, ppl = do_evaluation(meta_net, val_iter)
+            loss, ppl, nll, kld, w = do_evaluation(meta_net, val_iter)
             val_loss_before.append(math.exp(loss))
             val_batch_loss_before += loss
+            val_nll_loss_before += nll
+            val_kld_loss_before += kld
+            val_kld_weight_before += w
             # mate tuning
-            val_loss, val_ppl = do_learning_fix_step(meta_net, train_iter, val_iter, iterations=config.meta_iteration)
+            val_loss, val_ppl, val_nll, val_kld, val_weight = do_learning_fix_step(meta_net, train_iter, val_iter,
+                                                                                   iterations=config.meta_iteration)
             val_loss_meta.append(math.exp(val_loss.item()))
+
             val_batch_loss_after += val_loss
+            val_nll_loss_after += nll
+            val_kld_loss_after += kld
+            val_kld_weight_after += w
             # updated result
 
             meta_net.load_state_dict({name: weights_original[name] for name in weights_original})
@@ -242,9 +254,26 @@ for meta_iteration in range(config.epochs):
         writer.add_scalars('loss_meta', {'val_loss_meta': np.mean(val_loss_meta)}, meta_iteration)
 
         val_batch_loss_before /= 100
+        val_nll_loss_before /= 100
+        val_kld_loss_before /= 100
+        val_kld_weight_before /= 100
+
         val_batch_loss_after /= 100
+        val_nll_loss_after /= 100
+        val_kld_loss_after /= 100
+        val_kld_weight_after /= 100
+
         print('val_loss: {:.3f} ===> {:.3f}'.format(val_batch_loss_before, val_batch_loss_after))
-        print('------------------------------')
+        print('before fine-tuning: {:.3f} = {:.3f} + {:.3f} * {:.3f}'.format(
+            val_batch_loss_before,
+            val_nll_loss_before,
+            val_kld_weight_before,
+            val_kld_loss_before))
+        print('after fine-tuning: {:.3f} = {:.3f} + {:.3f} * {:.3f}'.format(
+            val_batch_loss_before,
+            val_nll_loss_before,
+            val_kld_weight_before,
+            val_kld_loss_before))
 
         # check early stop
         # if np.mean(val_loss_meta) < best_loss:
@@ -252,8 +281,10 @@ for meta_iteration in range(config.epochs):
             # best_loss = np.mean(val_loss_meta)
             best_loss = val_batch_loss_after
             stop_count = 0
-            meta_net.save_model(best_loss, meta_iteration, 0)
+            meta_net.save_model(best_loss, meta_iteration, global_step)
         else:
             stop_count += 1
+            print('remain patience: {}'.format(patience - stop_count))
             if stop_count > patience:
                 break
+        print('------------------------------')
